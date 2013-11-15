9b10b6822134cf2a3bdc3ee5d8468635c57d0e56
simpleframework <cknet@126.com>
2013-11-15 12:17:07 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/organization/impl/DepartmentService.java
+++ src/main/java/net/simpleframework/organization/impl/DepartmentService.java
@@ -11,8 +11,7 @@ import net.simpleframework.organization.OrganizationException;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885)
- *         https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class DepartmentService extends AbstractOrganizationService<IDepartment, Department>
