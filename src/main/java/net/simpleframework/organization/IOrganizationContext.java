4be014f567c8fbf4600e716e6e36ce299ebac229
simpleframework <cknet@126.com>
2013-12-10 10:54:59 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/organization/IOrganizationContext.java
+++ src/main/java/net/simpleframework/organization/IOrganizationContext.java
@@ -1,7 +1,7 @@
 package net.simpleframework.organization;
 
+import net.simpleframework.ctx.IADOModuleContext;
 import net.simpleframework.ctx.IModuleRef;
-import net.simpleframework.ctx.service.ado.db.IDbModuleContext;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -9,7 +9,7 @@ import net.simpleframework.ctx.service.ado.db.IDbModuleContext;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public interface IOrganizationContext extends IDbModuleContext {
+public interface IOrganizationContext extends IADOModuleContext {
 
 	static final String MODULE_NAME = "simple-module-organization";
 
