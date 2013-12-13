92ffd6986680b9e69562bc6a9302b785f16d26f2
simpleframework <cknet@126.com>
2013-12-13 13:17:57 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/organization/OrganizationException.java
+++ src/main/java/net/simpleframework/organization/OrganizationException.java
@@ -1,6 +1,6 @@
 package net.simpleframework.organization;
 
-import net.simpleframework.common.th.RuntimeExceptionEx;
+import net.simpleframework.ctx.ModuleException;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -8,15 +8,15 @@ import net.simpleframework.common.th.RuntimeExceptionEx;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public class OrganizationException extends RuntimeExceptionEx {
+public class OrganizationException extends ModuleException {
 	private static final long serialVersionUID = 6885538428759225872L;
 
 	public OrganizationException(final String msg, final Throwable cause) {
 		super(msg, cause);
 	}
 
-	public static RuntimeException of(final Throwable throwable) {
-		return _of(OrganizationException.class, null, throwable);
+	public static ModuleException of(final Throwable throwable) {
+		return (ModuleException) _of(OrganizationException.class, null, throwable);
 	}
 
 	public static OrganizationException of(final String msg) {
