e5b9eb651636638d8c31e7af78b913651376d17a
CHENK <CHENK@CHENK-PC>
2016-02-02 17:07:16 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/organization/OrganizationException.java
+++ src/main/java/net/simpleframework/organization/OrganizationException.java
@@ -5,11 +5,11 @@ import net.simpleframework.common.th.RuntimeExceptionEx;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885)
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class OrganizationException extends RuntimeExceptionEx {
-	private static final long serialVersionUID = 6885538428759225872L;
 
 	public OrganizationException(final String msg, final Throwable cause) {
 		super(msg, cause);
@@ -22,4 +22,6 @@ public class OrganizationException extends RuntimeExceptionEx {
 	public static OrganizationException of(final String msg) {
 		return _of(OrganizationException.class, msg);
 	}
+
+	private static final long serialVersionUID = 6885538428759225872L;
 }
